6f086773f2318dacf4ce1d4657d955ad492964b9
maram-ra <maramraddad33@gmail.com>
2025-02-13 16:23:37 +0000

    Added script

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,13 @@
  */
 int main(void)
 {
- printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
- printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
- printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
- printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
- printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
+printf("Size of a char: %u byte(s)\n", (unsigned int) sizeof(char));
+printf("Size of an int: %u byte(s)\n", (unsigned int) sizeof(int));
+printf("Size of a long int: %lu byte(s)\n",
+(unsigned long) sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n",
+(unsigned long) sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", (unsigned int) sizeof(float));
 
-return(0);
+return (0);
 }
