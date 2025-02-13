10ce8b4b97746f71b183d1f10aa0e62d1c90fe32
maram-ra <maramraddad33@gmail.com>
2025-02-13 15:52:28 +0000

    f

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various data types on the computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(char));
+ printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(int));
+ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+ printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(float));
+
+return(0);
+}
