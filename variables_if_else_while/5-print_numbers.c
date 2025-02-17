fc94b2a76245eef8fdbcbffde9f2b22c2362c77b
maram-ra <maramraddad33@gmail.com>
2025-02-17 14:28:55 +0000

    s

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+int c;
+
+for (c = '0'; c >= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+
+return (0);
+}
