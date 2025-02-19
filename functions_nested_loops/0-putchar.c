ea0a482d87f7d0257c5898b5c042a131e93550f1
maram-ra <maramraddad33@gmail.com>
2025-02-19 11:08:59 +0000

    Added script

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char str[] = "_putchar";
+
+int c;
+for (c = 0; c < 8; ++c)
+_putchar(str[c]);
+_putchar('\n');
+return (0);
+}
