3af6377d6f2ea1003bfae278b04e9165fb455278
maram-ra <maramraddad33@gmail.com>
2025-03-20 09:54:09 +0000

    Add

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,23 +1,17 @@
-section .data
-    message db "Hello, World", 10  ; Message to be printed with newline
-    msg_len equ $ - message        ; Calculate the length of the message automatically
-
 section .text
-    global _start
+global main
 
-_start:
-    ; Print "Hello, World\n" to the screen
-    ; syscall: sys_write (1)
-    mov rax, 1        ; sys_write -> syscall number
-    mov rdi, 1        ; stdout (file descriptor)
-    mov rsi, message  ; Address of the message
-    mov rdx, msg_len  ; Length of the message
-    syscall           ; Make the system call
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
 
-    ; Exit the program
-    ; syscall: sys_exit (60)
-    mov rax, 60       ; sys_exit -> syscall number
-    xor rdi, rdi      ; Exit code 0 (successful)
-    syscall           ; Make the system call
+	mov rax, 60
+	mov rdi, 0
+	syscall
 
-
+section .rodata
+	msg: db "Hello, World", 10
+	msglen: equ $ - msg
