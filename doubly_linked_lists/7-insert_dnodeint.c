dcfd4e8b167e9ab2db7f7b80ca2b641d209c9f40
maram-ra <maramraddad33@gmail.com>
2025-04-04 10:44:28 +0000

    Add

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
- * @h: A pointer to the head of the dlistint_t list.
- * @idx: The position to insert the new node.
- * @n: The integer for the new node to contain.
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to head of doubly linked list
+ * @idx: index to look for
+ * @n: numeric value wanted in node inserted
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * Return: address to new node or NULL if not possible to insert
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	dlistint_t *new_node, *tmpcount = *h, *tmp1 = *h, *tmp2;
+	unsigned int count = 0;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
-	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-	}
-
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!h)
+		return (NULL);
+	while (tmpcount)
+		tmpcount = tmpcount->next, count++;
+	if (idx > count)
 		return (NULL);
 
-	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-	return (new);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->prev = NULL, new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	for (count = 0; count < idx; count++)
+		tmp2 = tmp1, tmp1 = tmp1->next;
+
+	new_node->next = tmp1;
+	new_node->prev = tmp2;
+	tmp2->next = new_node;
+	if (tmp1)
+		tmp1->prev = new_node;
+
+	return (new_node);
 }
