3961310f796ee2a63681fff6f0bd2fe57dc6443b
maram-ra <maramraddad33@gmail.com>
2025-04-04 10:42:19 +0000

    Add

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,41 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a  new node at a given position.
- * @head: The first element of the list.
- * @idx: The position of insertion.
- * @n: The data of the new node.
- * Return: The address of the new node if success.
- *	   NULL otherwise.
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
-dlistint_t *insert_dnodeint_at_index(
-		dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *node;
-	unsigned int i = 0;
+	dlistint_t *tmp = *h, *new;
 
-	node = malloc(sizeof(dlistint_t));
-	if (node)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		node->n = n;
-		if (idx == 0 || head == NULL)
-		{
-			add_dnodeint(head, n);
-		}
-		else
-		{
-			tmp = *head;
-			while ((tmp != NULL) && (i + 1 < idx))
-			{
-				tmp = tmp->next;
-				i++;
-			}
-			if (i + 1  == idx)
-			{
-				if (tmp == NULL)
-				{
-					add_dnodeint_end(head, n);
-				}
-				else
-				{
-					node->prev = tmp;
-					node->next = tmp->next;
-					tmp->next = node;
-					(node->next)->prev = node;
-				}
-			}
-		}
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
-	return (node);
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
